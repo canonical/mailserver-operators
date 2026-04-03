385614924d9edc2f00e6014730cadc6f5acda648
Ali Ugur <aliugur997@gmail.com>
2026-04-03 14:01:30 +0300

    fix: integration tests

--- dovecot-charm/conftest.py
+++ dovecot-charm/conftest.py
@@ -7,6 +7,3 @@ import pytest
 def pytest_addoption(parser: pytest.Parser) -> None:
     """Register custom options for all pytest runs."""
     parser.addoption("--charm-file", action="store")
-    parser.addoption("--use-existing", action="store_true", default=False)
-    parser.addoption("--model", action="store", default=None)
-    parser.addoption("--keep-models", action="store_true", default=False)
